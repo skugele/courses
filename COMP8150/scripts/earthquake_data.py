05309b1a397bb7a4e1476ae900b9c67bf64970fb
skugele <seankugele@gmail.com>
2018-09-26 03:21:19 +0000

    Remove comments and change data path.

--- COMP8150/scripts/earthquake_data.py
+++ COMP8150/scripts/earthquake_data.py
@@ -1,9 +1,8 @@
-import numpy as np
-# from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data_path = '../../data/earthquake_db.txt'
+data_path = 'data/earthquake_db.txt'
 data = pd.read_csv(data_path, sep='\t')
 
 sample = data.sample(n=35, random_state=1)
@@ -14,14 +13,14 @@ lats = sample.loc[:,['LATITUDE']].values.astype(float)
 longs = sample.loc[:,['LONGITUDE']].values.astype(float)
 magnitudes = sample.loc[:,['EQ_MAG_MS']].values.astype(float)
 
-# m = Basemap(projection='hammer',lon_0=0)
-# x,y = m(longs, lats)
-# m.scatter(x,y,s=12,marker='x',color='r', zorder=10)
-# m.drawmapboundary(fill_color='#99ffff')
-# m.fillcontinents(color='#cc9966',lake_color='#99ffff')
-#
-# plt.title('Plot of the locations of 35 earthquake events randomly selected from the data set', fontsize=12)
-# plt.show()
+m = Basemap(projection='hammer',lon_0=0)
+x,y = m(longs, lats)
+m.scatter(x,y,s=12,marker='x',color='r', zorder=10)
+m.drawmapboundary(fill_color='#99ffff')
+m.fillcontinents(color='#cc9966',lake_color='#99ffff')
+
+plt.title('Plot of the locations of 35 earthquake events randomly selected from the data set', fontsize=12)
+plt.show()
 
 plt.gcf().clear()
 plt.title('Plot of Earthquake Magnitude for 35\nRandomly Selected Earthquake Events', fontsize=12)
