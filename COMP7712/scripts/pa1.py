d98936ba2bc8a9c28132d0a2bcb0c6dabd3bf0d6
skugele <seankugele@gmail.com>
2018-10-02 17:33:36 +0000

    Add complete synthetic_division function.  Add string representations for the polynomial class.  Add handling for rational numbers.

--- COMP7712/scripts/pa1.py
+++ COMP7712/scripts/pa1.py
@@ -1,5 +1,31 @@
 def synthetic_division(p1, p2):
-    pass
+    if not (isinstance(p1, Polynomial) and isinstance(p2, Polynomial)):
+        raise ValueError('Both arguments must be polynomials')
+
+    if p1 == p2:
+        return Polynomial('0 1'), 0
+
+    if p2.degree != 1:
+        raise ValueError('Divisor must be degree 1 polynomial')
+
+    if p2[1] != 1:
+        raise ValueError('Leading coefficient of divisor must be 1')
+
+    div = -p2[0]
+    result = []
+    for c in p1.coefficients:
+        if len(result) == 0:
+            result.append(c)
+        else:
+            result.append(c + div * result[-1])
+
+    remainder = result.pop()
+    poly_spec = []
+    for exp, coeff in zip(range(len(result) - 1, -1, -1), result):
+        poly_spec.append(exp)
+        poly_spec.append(coeff)
+
+    return Polynomial(poly_spec), remainder
 
 
 class Polynomial(object):
@@ -20,7 +46,36 @@ class Polynomial(object):
         self._terms = [0] * (int(self._spec[0]) + 1)
 
         for exp, coeff in zip(self._spec[0::2], self._spec[1::2]):
-            self._terms[int(exp)] = int(coeff)
+            self._terms[int(exp)] = float(coeff)
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        p_terms = []
+        for i, c in enumerate(self._terms):
+            c = int(c) if float(c) == int(c) else round(c, 2)
+
+            if c == 0:
+                continue
+
+            if int(i) == 0:
+                term = str(c)
+
+            else:
+                c = '' if c == 1 else c
+
+                if int(i) == 1:
+                    term = '{}x'.format(str(c))
+                else:
+                    term = '{}x^{}'.format(str(c), i)
+
+            p_terms.append(term)
+
+        result = ' + '.join(reversed(p_terms))
+        result = result.replace(' + -', ' - ')
+
+        return result
 
     def __getitem__(self, item):
         return self._terms[item]
@@ -28,17 +83,35 @@ class Polynomial(object):
     def __setitem__(self, key, value):
         self._terms[key] = value
 
+    def __eq__(self, other):
+        if isinstance(other, Polynomial):
+            return self._terms == other._terms
 
-def run_poly_div():
-    p_spec = input('Please specify a polynomial: ')
-    p1 = Polynomial(p_spec.split(' '))
+        return False
 
-    a = input('Please provide a number: ')
-    p2 = Polynomial([1, 1, 0, a])
+    def __len__(self):
+        return len(self._terms)
+
+    @property
+    def degree(self):
+        return len(self) - 1
+
+    @property
+    def coefficients(self):
+        for c in reversed(self._terms):
+            yield c
 
 
 if __name__ == '__main__':
     try:
-        run_poly_div()
+        p_spec = input('Please specify a polynomial: ')
+        p1 = Polynomial(p_spec.split(' '))
+
+        a = float(input('Please provide a number: '))
+        p2 = Polynomial([1, 1, 0, -a])
+
+        q, r = synthetic_division(p1, p2)
+        print('Quotient: {}\nRemainder: {}'.format(q, r))
+
     except Exception as e:
         print(e)
--- COMP7712/test/pa1_tests.py
+++ COMP7712/test/pa1_tests.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from pa1 import Polynomial
+from pa1 import Polynomial, synthetic_division
 
 
 class PolynomialTest(TestCase):
@@ -26,3 +26,86 @@ class PolynomialTest(TestCase):
         self.assertEqual(p3[3], 5)
         self.assertEqual(p3[1], 10)
         self.assertEqual(p3[0], 5)
+
+        # Rational coefficients
+        p3 = Polynomial('3 0.25 1 10 0 -.105')
+
+        self.assertEqual(p3[3], 0.25)
+        self.assertEqual(p3[1], 10)
+        self.assertEqual(p3[0], -.105)
+
+    def test_equality(self):
+        p1 = Polynomial('10 17 1 2 0 1')
+        self.assertEqual(p1, p1)
+
+        self.assertEqual(Polynomial('0 1'), Polynomial('0 1'))
+        self.assertEqual(Polynomial('3 4 2 5 1 7'), Polynomial('3 4 2 5 1 7'))
+        self.assertEqual(Polynomial('30 3 1 7'), Polynomial('30 3 1 7'))
+        self.assertEqual(Polynomial('30 3 1 7'), Polynomial('30 3 1 7'))
+
+        self.assertNotEqual(Polynomial('0 1'), '0 1')
+
+    def test_degree(self):
+        self.assertEqual(Polynomial('17 1 6 5 4 2').degree, 17)
+        self.assertEqual(Polynomial('0 1').degree, 0)
+        self.assertEqual(Polynomial('1 7').degree, 1)
+        self.assertEqual(Polynomial('3 -7 2 1 1 19 0 -2').degree, 3)
+
+    def test_iter(self):
+        p = Polynomial('13 2 9 5 6 -4 2 1 1 1 0 -2')
+
+        exp = p.degree
+        for c in p.coefficients:
+            self.assertEqual(p[exp], c)
+            exp -= 1
+
+        self.assertEqual(exp, -1)
+
+    def test_str(self):
+        p1 = Polynomial([3, 5])
+        self.assertEqual(str(p1), '5x^3')
+
+        p2 = Polynomial('13 2 9 5 6 -4 2 1 1 1 0 -2')
+        self.assertEqual(str(p2), '2x^13 + 5x^9 - 4x^6 + x^2 + x - 2')
+
+        p3 = Polynomial('13 2.0 9 5.173 6 -4 0 -0.25')
+        self.assertEqual(str(p3), '2x^13 + 5.17x^9 - 4x^6 - 0.25')
+
+
+class SyntheticDivisionTest(TestCase):
+
+    def test_valid(self):
+        p1 = Polynomial('1 1 0 1')
+        p2 = Polynomial('1 1 0 1')
+
+        q, r = synthetic_division(p1, p2)
+        self.assertEqual(q, Polynomial('0 1'))
+        self.assertEqual(r, 0)
+
+        p1 = Polynomial('4 3 2 7 1 -1 0 3')
+        p2 = Polynomial('1 1 0 -1')
+
+        q, r = synthetic_division(p1, p2)
+        self.assertEqual(q, Polynomial('3 3 2 3 1 10 0 9'))
+        self.assertEqual(r, 12)
+
+        p1 = Polynomial('3 1 2 -12 0 -42')
+        p2 = Polynomial('1 1 0 -3')
+
+        q, r = synthetic_division(p1, p2)
+        self.assertEqual(q, Polynomial('2 1 1 -9 0 -27'))
+        self.assertEqual(r, -123)
+
+        p1 = Polynomial('5 42 3 -19 0 -13')
+        p2 = Polynomial('1 1 0 20')
+
+        q, r = synthetic_division(p1, p2)
+        self.assertEqual(q, Polynomial('4 42 3 -840 2 16781 1 -335620 0 6712400'))
+        self.assertEqual(r, -134248013)
+
+    def test_invalid(self):
+        self.assertRaises(ValueError, synthetic_division, Polynomial('1 2'), 2)
+        self.assertRaises(ValueError, synthetic_division, 2, Polynomial('1 2'))
+        self.assertRaises(ValueError, synthetic_division, 2, 2)
+
+        self.assertRaises(ValueError, synthetic_division, Polynomial('3 5 1 2'), Polynomial('2 1 0 1'))
