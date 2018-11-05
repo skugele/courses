306a056a4f88ac99e3bce8b467afa175704056cd
skugele <11bwai$mmim1>
2018-11-05 17:34:08 -0600

    Add plot for all RGB probability dist.

--- COMP8150/project/ros/workspace/src/wanderbot/src/multi_regress.py
+++ COMP8150/project/ros/workspace/src/wanderbot/src/multi_regress.py
@@ -12,7 +12,6 @@ from sklearn.linear_model import LinearRegression
 from mpl_toolkits import mplot3d
 
 
-
 # def luminosity(image):
 #     rgb_values = np.reshape(image, (-1, 3))
 #     scaled_rgb = rgb_values / np.float(255)
@@ -107,28 +106,47 @@ reg = LinearRegression().fit(X, y)
 
 # plot plane
 
-x1_space = np.linspace(0, 50)
-x2_space = np.linspace(-1000, 10000)
+# x1_space = np.linspace(0, 50)
+# x2_space = np.linspace(-1000, 10000)
+#
+#
+# def linear_model(reg, x1, x2):
+#     return reg.intercept_ + x1 * reg.coef_[0] + x2 * reg.coef_[1]
+#
+#
+# X, Y = np.meshgrid(x1_space, x2_space)
+# Z = linear_model(reg, X, Y)
+#
+# fig = plt.figure()
+# ax = Axes3D(fig)
+#
+# # ax.set_zlim(-0.2, 1.2)
+#
+# ax.set_xlabel(r'$x_1$')
+# ax.set_ylabel(r'$x_2$')
+# ax.set_zlabel(r'$y$')
+# ax.set_title('Multi-Regression Hyper-Plane with Scatter Plot')
+#
+# ax.plot_surface(X, Y, Z, rstride=5, cstride=5, cmap='Blues', edgecolor=ColorConverter().to_rgba('black', alpha=0.4), alpha=0.4)
+# ax.scatter(x_1, x_2, y, c=cs)
+#
+# plt.show()
 
+cs = []
+y = []
+x_1 = []
+x_2 = []
 
-def linear_model(reg, x1, x2):
-    return reg.intercept_ + x1 * reg.coef_[0] + x2 * reg.coef_[1]
+for spec in image_specs[250:-1]:
+    cs.append(int(spec.category))
+    y.append(1 if spec.category == '6' else 0)
+    x_1.append(np.abs(calc_l_avg(spec) - avg_l_avgs['6']))
 
+    rgbs, _, _ = get_unique_rgbs_for_specs([spec])
+    x_2.append(rgb_distance_from_cluster(rgbs, cmp_rgbs))
 
-X, Y = np.meshgrid(x1_space, x2_space)
-Z = linear_model(reg, X, Y)
+X = np.column_stack((x_1, x_2))
+pred = reg.predict(X)
 
-fig = plt.figure()
-ax = Axes3D(fig)
-
-# ax.set_zlim(-0.2, 1.2)
-
-ax.set_xlabel(r'$x_1$')
-ax.set_ylabel(r'$x_2$')
-ax.set_zlabel(r'$y$')
-ax.set_title('Multi-Regression Hyper-Plane with Scatter Plot')
-
-ax.plot_surface(X, Y, Z, rstride=5, cstride=5, cmap='Blues', edgecolor=ColorConverter().to_rgba('black', alpha=0.4), alpha=0.4)
-ax.scatter(x_1, x_2, y, c=cs)
-
-plt.show()
+for y_true, y_pred in zip(y, pred):
+    print('{},{}'.format(str(y_pred),str(y_true)))
