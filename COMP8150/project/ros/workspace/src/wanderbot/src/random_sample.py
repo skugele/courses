3cb3262e5cb01210c24c79e709b6bdf01d1008eb
skugele <11bwai$mmim1>
2018-11-05 18:10:06 -0600

    Add synthetic images and random_sample script.

--- COMP8150/project/ros/workspace/src/wanderbot/src/random_sample.py
+++ COMP8150/project/ros/workspace/src/wanderbot/src/random_sample.py
@@ -1,14 +1,5 @@
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-
-from matplotlib.colors import Normalize
-
-import matplotlib.cm as cmx
-
-import numpy as np
-import cv2
-
 from image_util import *
+from distance_metric import *
 
 image_specs = get_image_specs(images_dir, labels=get_categories(labels_file), scaling_factor=0.1)
 images_for_object = {c: [] for c in map(str, range(1, 7))}
@@ -30,38 +21,44 @@ uniq_counts = uniq_counts[sorted_order_indices]
 total_values = np.sum(uniq_counts)
 prob = uniq_counts / np.double(np.sum(uniq_counts))
 
-# indices = np.random.choice(np.arange(0, len(prob)), size=(640 * 480), p=prob)
-# chosen_rgb = uniq_rgb_values[indices]
+for c in map(str, range(1, 7)):
+    specs = find_image_specs_by_category(image_specs, c)
+    rgb_values, counts, prob = get_unique_rgbs_for_specs(specs)
+    sorted_order_indices = np.asarray(list(reversed(np.argsort(counts))))
 
-# img = np.reshape(chosen_rgb, (480,640,3))
-# img_sorted = np.sort(img, axis=2)
-# cv2.imshow("image", img_sorted)
-# cv2.waitKey()
+    for i in range(1, 51):
+        indices = np.random.choice(np.arange(0, len(prob)), size=(640 * 480), p=prob)
+        chosen_rgb = rgb_values[indices]
 
-
+        img = np.reshape(chosen_rgb, (480, 640, 3))
+        img_sorted = np.sort(img, axis=2)
+        cv2.imshow('{}_{}'.format(c, i), img_sorted)
+        filename = '{}_{}.png'.format('synthetic',str(i))
+        cv2.imwrite('/'.join(['/var/local/data/skugele/COMP8150/project/synthetic_images', c, filename]), img_sorted)
+        # cv2.waitKey(3)
 
 # ax = fig.add_subplot(111, projection='3d')
 
-xs = uniq_rgb_values[:, 0]
-ys = uniq_rgb_values[:, 1]
-zs = uniq_rgb_values[:, 2]
-
-cm = plt.get_cmap('jet')
-c_norm = Normalize(vmin=min(prob), vmax=max(prob))
-scalar_map = cmx.ScalarMappable(norm=c_norm, cmap=cm)
-fig = plt.figure()
-ax=Axes3D(fig)
-scalar_map.set_array(prob)
-# ax.scatter(xs, ys, zs, c=prob, cmap=plt.cm.get_cmap('Accent'))
-ax.scatter(xs, ys, zs, s=prob*15000, c=scalar_map.to_rgba(prob), alpha=0.6, cmap=cm)
-
-fig.colorbar(scalar_map)
-
-ax.set_xlabel('Blue')
-ax.set_ylabel('Green')
-ax.set_zlabel('Red')
-
-plt.show()
+# xs = uniq_rgb_values[:, 0]
+# ys = uniq_rgb_values[:, 1]
+# zs = uniq_rgb_values[:, 2]
+#
+# cm = plt.get_cmap('jet')
+# c_norm = Normalize(vmin=min(prob), vmax=max(prob))
+# scalar_map = cmx.ScalarMappable(norm=c_norm, cmap=cm)
+# fig = plt.figure()
+# ax=Axes3D(fig)
+# scalar_map.set_array(prob)
+# # ax.scatter(xs, ys, zs, c=prob, cmap=plt.cm.get_cmap('Accent'))
+# ax.scatter(xs, ys, zs, s=prob*15000, c=scalar_map.to_rgba(prob), alpha=0.6, cmap=cm)
+#
+# fig.colorbar(scalar_map)
+#
+# ax.set_xlabel('Blue')
+# ax.set_ylabel('Green')
+# ax.set_zlabel('Red')
+#
+# plt.show()
 # plt.savefig(pad_inches=0.2, fname='/home/skugele/Development/courses/COMP8150/project/figures/No Objects Category 3D Probability Scatter Plot.png')
 # #
 
